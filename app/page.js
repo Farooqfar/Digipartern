48a80e3202c3a6acbce6bd36d2c4b1685d8999e9
Farooqfar <info4895@gmail.com>
2025-12-28 12:58:27 +0500

    agency

--- app/page.js
+++ app/page.js
@@ -2,64 +2,50 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <main className="w-full h-screen">
+      <section className="w-full h-full relative">
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
+          src="/ban.png"
+          alt="banner"
+          fill
+          className="object-cover"
           priority
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+        <div className="absolute w-full">
+          <nav className="w-full flex justify-between items-center p-5">
+            <ul>
+              <li className="bg-white/10 backdrop-blur-sm rounded-full w-20 p-1">
+                <Image
+                  src="/logo.png"
+                  alt="logo"
+                  width={100}
+                  height={100}
+                  className="w-15"
+                />
+              </li>
+            </ul>
+            <ul className="w-100 flex gap-10 border-white border rounded-4xl justify-center items-center p-1">
+              <li className="bg-white text-black w-20 text-center p-3 rounded-4xl">
+                Home
+              </li>
+              <li className=" text-black w-20 text-center p-3 rounded-4xl">
+                Services
+              </li>
+              <li className=" text-black w-20 text-center p-3 rounded-4xl">
+                Contact
+              </li>
+            </ul>
+          </nav>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        <div className="absolute left-32 bottom-0 w-80 h-70 bg-white/10 backdrop-blur-md rounded-t-full flex flex-col justify-center items-center gap-10">
+          <div></div>
+          <div>
+            <h1 className="text-8xl text-white font-medium">100+</h1>
+            <p className="text-white">Successful project around the world</p>
+          </div>
         </div>
-      </main>
-    </div>
+        <div className="absolute bottom-0 w-14 h-100 left-4 rounded-t-full flex justify-between items-center p-5 bg-white/10 backdrop-blur-md"></div>
+      </section>
+    </main>
   );
 }
